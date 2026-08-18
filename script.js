ea4e5078b29f5e293883de6f532a975c015fd765
Edlawit Tsegaye <edlawitdigital3@gmail.com>
2026-08-18 12:16:03 +0300

    add a function that can calculate grade

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ const course = "javascript";
 const score = 98;
 const attendance = 90;
 
+//====================================================
+const calculateGrade =(score) => {
+  if (score >= 90){
+    return "A";
+  }
+  else if (score >=80){
+    return "B";
+  }
+  else if (score >=70){
+    return "C";
+  }
+  else if (score >=60){
+    return "D";
+  }
+  else{
+    return "F"
+  }
+};
+const grade = calculateGrade(score);
+console.log("Grade:", grade);
+document.getElementById("grade").textContent = grade;
+
